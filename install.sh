9ce143c15a45a5a8219d1fb50e4f3046d2a8784c
Tomoyuki Sakurai <y@trombik.org>
2018-02-11 10:37:34 +0900

    add script to install ansible from ports

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+PORTSDIR="/usr/ports"
+BUILD_DEPENDS="gtar py-sphinx"
+
+while read line; do
+    rm -rf "${PORTSDIR}/${line}"
+done < overrides.txt
+cp -R ports/* ${PORTSDIR}/
+
+pkg_add ${BUILD_DEPENDS}
+make -C ${PORTSDIR}/sysutils/ansible install clean
