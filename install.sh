1864bd07b575cf8c86c6e70a06a015c350be739a
Tomoyuki Sakurai <y@trombik.org>
2018-02-11 10:58:49 +0900

    missing `uname -r`

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ PREFIX="/usr"
 PORTSDIR="${PREFIX}/ports"
 BUILD_DEPENDS="gtar-- py-sphinx"
 
-ftp -o - `cat /etc/installurl`/ports.tar.gz | tar -C ${PREFIX} -zxf -
+ftp -o - `cat /etc/installurl`/`uname -r`/ports.tar.gz | tar -C ${PREFIX} -zxf -
 while read line; do
     rm -rf "${PORTSDIR}/${line}"
 done < overrides.txt
